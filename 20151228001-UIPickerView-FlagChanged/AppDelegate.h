3b5110706ebec79294eda4ff124868d78d563542
raineryue <jinlei_yue@sina.com>
2015-12-28 14:28:29 +0800

    Initial Commit

---
+++ 20151228001-UIPickerView-FlagChanged/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  20151228001-UIPickerView-FlagChanged
+//
+//  Created by Rainer on 15/12/28.
+//  Copyright © 2015年 Rainer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
